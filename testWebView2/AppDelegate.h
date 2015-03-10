df0a12df02dc6e3fedc22934a87e0eff58b67490
hudamin <hudamin@baidu.com>
2015-03-10 20:02:15 +0800

    Initial Commit

---
+++ testWebView2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testWebView2
+//
+//  Created by hudamin on 15/3/10.
+//  Copyright (c) 2015年 com.258i. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testWebView2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testWebView2
+//
+//  Created by hudamin on 15/3/10.
+//  Copyright (c) 2015年 com.258i. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
