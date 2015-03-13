581dafef1af2701ff81d5b613bc067f3ddfce5b9
hudamin <hudamin@baidu.com>
2015-03-13 16:52:57 +0800

    master: add swipe recognizers for web view

--- MyBrowser/ViewController.m
+++ MyBrowser/ViewController.m
@@ -29,12 +29,21 @@
     [self.backBtn addTarget:self action:@selector(backBtnClick:) forControlEvents:UIControlEventTouchDown];
     [self.nextBtn addTarget:self action:@selector(nextBtnClick:) forControlEvents:UIControlEventTouchDown];
     
+    [self initRecognizers];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+
+
+
 
 - (IBAction)changeText:(id)sender {
     NSLog(@"change Text");
@@ -55,6 +64,9 @@
 
 
 
+// Util methods
+
+
 - (void) loadURL {
     self.text = self.textField.text;
     NSURL *url = [NSURL URLWithString:self.text];
@@ -70,23 +82,68 @@
     NSLog(@"Update URL");
 }
 
-
-
-// Navigation buttons
-- (IBAction)backBtnClick:(id)sender {
-    NSLog(@"Back Button Click");
+- (void) goBack {
     [self.webView goBack];
-
     [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateURL:) userInfo: nil repeats:NO];
+}
+
+- (void) goForward {
+    [self.webView goForward];
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateURL:) userInfo: nil repeats:NO];
+}
+
+- (void) initRecognizers {
+    UISwipeGestureRecognizer *swipeRightRecognizer
+    = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(webViewSwipeRight:)];
+    
+    // Take no effects
+    // self.view.gestureRecognizers = [self.view.gestureRecognizers arrayByAddingObject:swipeRightRecognizer];
+    // The right way to add a new recognizer
+    [self.webView addGestureRecognizer:swipeRightRecognizer];
+    
+    [swipeRightRecognizer setNumberOfTouchesRequired:1];
+    [swipeRightRecognizer setDirection:UISwipeGestureRecognizerDirectionRight];
+    
+    UISwipeGestureRecognizer *swipeLeftRecognizer
+    = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(webViewSwipeLeft:)];
+    
+    [self.webView addGestureRecognizer:swipeLeftRecognizer];
+    
+    [swipeLeftRecognizer setNumberOfTouchesRequired:1];
+    [swipeLeftRecognizer setDirection:UISwipeGestureRecognizerDirectionLeft];
+}
+
+
+
+
+
+
+
+
+// Navigation buttons' Actions
+- (IBAction)backBtnClick:(id)sender {
+    [self goBack];
 }
 
 - (IBAction)nextBtnClick:(id)sender {
-    NSLog(@"Next Button Click");
-    [self.webView goForward];
-    
-    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateURL:) userInfo: nil repeats:NO];
+    [self goForward];
 }
 
+
+
+
+
+// WebView Swipe Handler
+
+- (void) webViewSwipeRight:(UIGestureRecognizer *)gestureRecognizer {
+    NSLog(@"right");
+    [self goBack];
+}
+
+- (void) webViewSwipeLeft:(UIGestureRecognizer *)gestureRecognizer {
+    NSLog(@"left");
+    [self goForward];
+}
 
 
 
